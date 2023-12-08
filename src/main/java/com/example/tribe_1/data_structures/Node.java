b6749204f988433626e8623157866f5c4edae8ce
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2023-12-08 22:34:23 +0500

    Updated Node

--- src/main/java/com/example/tribe_1/data_structures/Node.java
+++ src/main/java/com/example/tribe_1/data_structures/Node.java
@@ -3,10 +3,10 @@ package com.example.tribe_1.data_structures;
 public class Node<T> {
 
     T data;
-    Node next;
+    Node<T> next;
 
     public Node(T data) {
         this.data = data;
-        next = null;
+        this.next = null;
     }
 }
