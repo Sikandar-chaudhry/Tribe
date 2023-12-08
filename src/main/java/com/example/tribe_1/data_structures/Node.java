6f29036195f093430766628f0eecef984558a8f5
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2023-12-08 22:20:39 +0500

    Created Node in data structures package

--- src/main/java/com/example/tribe_1/data_structures/Node.java
+++ src/main/java/com/example/tribe_1/data_structures/Node.java
@@ -1,7 +1,12 @@
 package com.example.tribe_1.data_structures;
 
-public class Node {
+public class Node<T> {
 
+    T data;
+    Node next;
 
-
+    public Node(T data) {
+        this.data = data;
+        next = null;
+    }
 }
