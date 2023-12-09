d4032a4fcad2f9f6d314e30d871c870f67ccc4eb
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2023-12-09 22:31:48 +0500

    Updated MainController.java

--- src/main/java/com/example/tribe_1/MainController.java
+++ src/main/java/com/example/tribe_1/MainController.java
@@ -10,26 +10,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/home")
-    public String showHomePage(){
-        return "home";
+    @GetMapping("/signin")
+    public String showLandingPage(){
+        return "signin";
     }
 
-    @GetMapping("/users")
-    public String displayUsers(Model model){
-        User user1 = new User(1,"s", "sikander", "1234");
-        User user2 = new User(2,"si", "ali", "5678");
-        User user3 = new User(3,"sik", "asim", "9101");
-        User user4 = new User(4,"sika", "hamza", "1213");
-        UserStorage userStorage = new UserStorage();
-        userStorage.addUser(user1);
-        userStorage.addUser(user2);
-        userStorage.addUser(user3);
-        userStorage.addUser(user4);
-        LinkedList<User> users = UserStorage.getUsers();
-        model.addAttribute("listUsers", users);
-        return "userDisplay";
+    @GetMapping("/signup")
+    public String showSignUpPage(){
+        return "signup";
     }
+
+//    @GetMapping("/users")
+//    public String displayUsers(Model model){
+//        User user1 = new User(1,"s", "sikander", "1234");
+//        User user2 = new User(2,"si", "ali", "5678");
+//        User user3 = new User(3,"sik", "asim", "9101");
+//        User user4 = new User(4,"sika", "hamza", "1213");
+//        UserStorage userStorage = new UserStorage();
+//        userStorage.addUser(user1);
+//        userStorage.addUser(user2);
+//        userStorage.addUser(user3);
+//        userStorage.addUser(user4);
+//        LinkedList<User> users = UserStorage.getUsers();
+//        model.addAttribute("listUsers", users);
+//        return "userDisplay";
+//    }
 
 
 }
