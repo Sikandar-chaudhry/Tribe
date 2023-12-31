8d95a3d0a9041d151f0b780240bf56e55bec2e7f
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2023-12-31 21:06:23 +0500

    Removes commented code.

--- src/main/java/com/example/tribe_1/MainController.java
+++ src/main/java/com/example/tribe_1/MainController.java
@@ -27,19 +27,3 @@ public class MainController {
 
 }
 
-//    @GetMapping("/users")
-//    public String displayUsers(Model model){
-//        User user1 = new User(1,"s", "sikander", "1234");
-//        User user2 = new User(2,"si", "ali", "5678");
-//        User user3 = new User(3,"sik", "asim", "9101");
-//        User user4 = new User(4,"sika", "hamza", "1213");
-//        UserStorage userStorage = new UserStorage();
-//        userStorage.addUser(user1);
-//        userStorage.addUser(user2);
-//        userStorage.addUser(user3);
-//        userStorage.addUser(user4);
-//        LinkedList<User> users = UserStorage.getUsers();
-//        model.addAttribute("listUsers", users);
-//        return "userDisplay";
-//    }
-
